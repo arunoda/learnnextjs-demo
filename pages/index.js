0aefee3044e3aa36b4e5c3b084455f7a46d99ad0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-03-22 11:25:39 +0530

    Add the link between about and index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+// This is Link API
+import Link from 'next/link'
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 )
