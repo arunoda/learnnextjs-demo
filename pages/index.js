c88e7c2d5c41c8e345a0ef401f4b08b6a5dfedf6
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-04-27 20:53:03 -0700

    Add index.js content without sub components.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 
-const PostLink = (props) => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-)
+function getPosts () {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js'},
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
+    { id: 'deploy-nextjs', title: 'Deploy apps with Zeit'},
+  ]
+}
 
 export default () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js"/>
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+      {getPosts().map((post) => (
+        <li>
+          <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+            <a>{post.title}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </Layout>
 )
