ec44555d68352b4a0ed480d7d02b2e1131fe9edf
lightswarm124 <lightswarm@gmail.com>
2017-11-27 00:07:09 -0500

    what a mess

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,6 @@ import Layout from '../components/MyLayout.js'
 
 export default () => (
     <Layout>
-       <p>This is the about page</p>
+       <p>This is the about page. We are learning Next.JS</p>
     </Layout>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 
-const PostLink = (props) => (
+function getPosts () {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js'},
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
+  ]
+}
+
+const PostLink = ({ post }) => (
   <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 )
 
@@ -13,9 +37,32 @@ export default () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js"/>
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+      {getPosts().map((post) => (
+        <PostLink key={post.id} post={post}/>
+      ))}
     </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 )
