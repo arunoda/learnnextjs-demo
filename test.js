c8dcf984a9419b2dec03909dfe187d1ae0142a1c
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-17 11:34:59 +0100

    Use Destructuring Assignment with the Rest Operator to Reassign Array Elements

--- test.js
+++ test.js
@@ -12,8 +12,24 @@
 
 //Use Destructuring Assignment to Assign Variables from Arrays
 
-let a= 8;
-let b = 6;
+// let a= 8;
+// let b = 6;
 
- [a,b]=[b,a]
-console.log(b)
+//  [a,b]=[b,a]
+// console.log(b)
+
+//ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
+
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  let arr,a,b;
+  [a,b,...arr]=source
+ 
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
