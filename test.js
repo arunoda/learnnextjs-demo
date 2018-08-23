3066c82c125b4743d49bb7d821b030782f219e6d
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-23 10:42:22 +0100

    Create Strings using Template Literals

--- test.js
+++ test.js
@@ -57,3 +57,29 @@
 //   })();
 //   console.log(stats); // should be object
 //   console.log(half(stats)); // should be 28.015
+
+//Create Strings using Template Literals
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray 
+  =[`<li class="text-warning">${arr[0]}</li>`,
+  `<li class="text-warning">${arr[1]}</li>`,
+  `<li class="text-warning">${arr[2]}</li>`];
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`, 
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
