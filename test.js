0d8e7d49a9ed4f8a7b009842ce02f3f41d10d82d
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-23 10:43:39 +0100

    comment new function

--- test.js
+++ test.js
@@ -59,27 +59,27 @@
 //   console.log(half(stats)); // should be 28.015
 
 //Create Strings using Template Literals
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["id-blacklist", "no-dup-keys"]
-};
-function makeList(arr) {
-  "use strict";
+// const result = {
+//   success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//   failure: ["no-var", "var-on-top", "linebreak"],
+//   skipped: ["id-blacklist", "no-dup-keys"]
+// };
+// function makeList(arr) {
+//   "use strict";
 
-  // change code below this line
-  const resultDisplayArray 
-  =[`<li class="text-warning">${arr[0]}</li>`,
-  `<li class="text-warning">${arr[1]}</li>`,
-  `<li class="text-warning">${arr[2]}</li>`];
-  // change code above this line
+//   // change code below this line
+//   const resultDisplayArray 
+//   =[`<li class="text-warning">${arr[0]}</li>`,
+//   `<li class="text-warning">${arr[1]}</li>`,
+//   `<li class="text-warning">${arr[2]}</li>`];
+//   // change code above this line
 
-  return resultDisplayArray;
-}
-/**
- * makeList(result.failure) should return:
- * [ `<li class="text-warning">no-var</li>`,
- *   `<li class="text-warning">var-on-top</li>`, 
- *   `<li class="text-warning">linebreak</li>` ]
- **/
-const resultDisplayArray = makeList(result.failure);
+//   return resultDisplayArray;
+// }
+// /**
+//  * makeList(result.failure) should return:
+//  * [ `<li class="text-warning">no-var</li>`,
+//  *   `<li class="text-warning">var-on-top</li>`, 
+//  *   `<li class="text-warning">linebreak</li>` ]
+//  **/
+// const resultDisplayArray = makeList(result.failure);
