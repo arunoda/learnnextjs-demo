2be458e8e5855a45553f27f48644353f67446077
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-23 10:41:53 +0100

    comment old test

--- test.js
+++ test.js
@@ -1,59 +1,59 @@
-// let a ={a:{b:{c:1}}}
+// // let a ={a:{b:{c:1}}}
 
-// let b = a?.a;
-// console.log(b)
-
-
-// var user = { name: 'Joe' }
-// var zip = user?.address?.zip;
-// console.log(zip)
-
-// var street = user.address && user.address.street;
-
-//Use Destructuring Assignment to Assign Variables from Arrays
-
-// let a= 8;
-// let b = 6;
-
-//  [a,b]=[b,a]
-// console.log(b)
-
-//ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
+// // let b = a?.a;
+// // console.log(b)
 
 
-const source = [1,2,3,4,5,6,7,8,9,10];
-function removeFirstTwo(list) {
-  "use strict";
-  // change code below this line
-  let arr,a,b;
-  [a,b,...arr]=source
+// // var user = { name: 'Joe' }
+// // var zip = user?.address?.zip;
+// // console.log(zip)
+
+// // var street = user.address && user.address.street;
+
+// //Use Destructuring Assignment to Assign Variables from Arrays
+
+// // let a= 8;
+// // let b = 6;
+
+// //  [a,b]=[b,a]
+// // console.log(b)
+
+// //ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
+
+
+// const source = [1,2,3,4,5,6,7,8,9,10];
+// function removeFirstTwo(list) {
+//   "use strict";
+//   // change code below this line
+//   let arr,a,b;
+//   [a,b,...arr]=source
  
-  return arr;
-}
-const arr = removeFirstTwo(source);
-console.log(arr); // should be [3,4,5,6,7,8,9,10]
-console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
+//   return arr;
+// }
+// const arr = removeFirstTwo(source);
+// console.log(arr); // should be [3,4,5,6,7,8,9,10]
+// console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
 
-//ES6: Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// //ES6: Use Destructuring Assignment to Pass an Object as a Function's Parameters
 
-const stats = {
-    max: 56.78,
-    standard_deviation: 4.34,
-    median: 34.54,
-    mode: 23.87,
-    min: -0.75,
-    average: 35.85
-  };
-  const half = (function() {
-    "use strict"; // do not change this line
+// const stats = {
+//     max: 56.78,
+//     standard_deviation: 4.34,
+//     median: 34.54,
+//     mode: 23.87,
+//     min: -0.75,
+//     average: 35.85
+//   };
+//   const half = (function() {
+//     "use strict"; // do not change this line
   
-    // change code below this line
-    return function half({max,min}) {
-      // use function argument destructuring
-      return (max + min) / 2.0;
-    };
-    // change code above this line
+//     // change code below this line
+//     return function half({max,min}) {
+//       // use function argument destructuring
+//       return (max + min) / 2.0;
+//     };
+//     // change code above this line
   
-  })();
-  console.log(stats); // should be object
-  console.log(half(stats)); // should be 28.015
+//   })();
+//   console.log(stats); // should be object
+//   console.log(half(stats)); // should be 28.015
