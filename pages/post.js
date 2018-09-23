b6242943e010ace65ab4d714fc1aa73a1ba1cc02
venus <venus@venusworld.cn>
2018-09-23 19:45:52 +0800

    Fix the 'url' property is deprecated warning

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,9 @@
+import { withRouter } from 'next/router';
 import Layout from '../components/MyLayout.js'
 
-export default (props) => (
+export default withRouter((props) => (
     <Layout>
-       <h1>{props.url.query.title}</h1>
+       <h1>{props.router.query.title}</h1>
        <p>This is the blog post content.</p>
     </Layout>
-)
+))
