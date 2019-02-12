700ececd8157bdb99ab4fe6ab0ade24485ac5ffe
luc <luc.leray@gmail.com>
2019-02-12 10:05:55 +0800

    add 4-create-dynamic-pages

---
+++ 4-create-dynamic-pages/pages/index.js
@@ -0,0 +1,21 @@
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js"/>
+      <PostLink title="Learn Next.js is awesome"/>
+      <PostLink title="Deploy apps with Zeit"/>
+    </ul>
+  </Layout>
+)
