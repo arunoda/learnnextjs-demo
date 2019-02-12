68c9a2e5968ce296c656295b5a89c0c9267d72e8
luc <luc.leray@gmail.com>
2019-02-12 10:02:35 +0800

    add 2-navigate-between-pages

---
+++ 2-navigate-between-pages/pages/index.js
@@ -0,0 +1,13 @@
+// This is Link API
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+)
+
+export default Index
