24b7167e0d2a91c266f534aa8ab86b7b54bf74c3
luc <luc.leray@gmail.com>
2019-02-12 10:13:52 +0800

    add 7-data-fetching

---
+++ 7-data-fetching/pages/index.js
@@ -0,0 +1,31 @@
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Index = (props) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('http://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
+  }
+}
+
+export default Index
---
+++ 7-data-fetching/pages/post.js
@@ -0,0 +1,22 @@
+import Layout from '../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
+
+const Post =  (props) => (
+    <Layout>
+       <h1>{props.show.name}</h1>
+       <p>{props.show.summary.replace(/<[\/]?p>/g, '')}</p>
+       <img src={props.show.image.medium}/>
+    </Layout>
+)
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`http://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default Post
